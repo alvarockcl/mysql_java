2be83985f808bba3fd9530083f7c6f98eb20111f
Alvaro Fuentes <alvarofue@gmail.com>
2018-07-22 21:20:38 -0400

    Inicia código

---
+++ src/main/java/cl/fuentes/app/App.java
@@ -0,0 +1,14 @@
+package cl.fuentes.app;
+
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+		// Aquí el código
+    	System.out.println("--------------------------------------------");
+    	System.out.println("Hola Mundo!!!");
+    	System.in.read();
+    	System.out.println("--------------------------------------------");
+    }
+
+}
