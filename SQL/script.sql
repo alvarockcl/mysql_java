ed25a2285db4ab583824c2175799cc803c6d6b49
Alvaro Fuentes <alvarofue@gmail.com>
2018-07-28 16:01:28 -0400

    Actualiza script

--- SQL/script.sql
+++ SQL/script.sql
@@ -1,9 +1,33 @@
-// Crear la base de datos
+-- ***********************************************************************************
+-- Ejemplo Mysql con Java
+-- ***********************************************************************************
 
-// Crear el usuario
+-- Crear la base de datos
+DROP DATABASE `ventas`;
+CREATE SCHEMA `ventas` DEFAULT CHARACTER SET utf8 ;
+USE ventas;
 
-// Dar privilegios
+-- Crear usuario base de datos
+CREATE USER usuventas IDENTIFIED BY 'passventas';
 
-// Crear tabla
+-- Dar privilegios
+grant usage on *.* to usuventas@localhost identified by 'passventas';
+grant all privileges on ventas.* to usuventas@localhost;
 
-// insertar datos
+-- Crear tabla usuario
+DROP TABLE usuario;
+CREATE TABLE usuario(
+	idusuario INT NOT NULL AUTO_INCREMENT,
+    usuario VARCHAR(50),
+    clave VARCHAR(50),
+    nombre VARCHAR(100),
+    tipousuario VARCHAR(50),
+    estado VARCHAR(1),
+    PRIMARY KEY (idusuario)
+);
+
+-- insertar datos
+INSERT INTO usuario (usuario, clave, nombre, tipousuario, estado) values
+('afuentes','1234','Administrador','Alvaro Fuentes','V');
+
+-- ***********************************************************************************
